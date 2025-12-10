e697ee321e51feeb186e1e6f175c3f247beb9c64
myork01 <sabom2nd@gmail.com>
2025-12-09 22:28:01 -0500

    variables

---
+++ scripts/variables.js
@@ -0,0 +1,60 @@
+console.log("My script is working");
+
+// String value
+let firstName = "Mark";
+let lastName = "York";
+// Numerical value
+let myAge = 34;
+let myHeight = 167;
+// Boolean value
+let isProfessor = true;
+let isStudent = false;
+
+//display the values
+console.log(firstName);
+console.log(lastName);
+console.log(myHeight);
+console.log(myAge);
+console.log(isProfessor);
+console.log(isStudent);
+
+//concatonation
+console.log("Last Name: " + lastName);
+console.log("Height: " + myHeight + "cm");
+
+//Arithmetic operations
+let num1 = 10;
+let num2 = 20;
+
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mul = num1 * num2;
+let div = num1 / num2
+
+
+
+
+
+let pi = 3.1416;
+let radius= 3;
+let circleArea = pi * radius * radius;
+
+console.log(`The areas of a circle with radius ${radius} is ${circleArea}.`);
+
+//difference between variable and constant
+//create the variable
+let aVariable;
+// assign the value
+aVariable = 30;
+// create and assign the variable
+let anotherVariable = 50;
+console.log(aVariable, anotherVariable)
+
+const weekDays = 7;
+console.log(weekDays);
+let vacationDays = 14;
+
+let totalDays = weekDays  + vacationDays;
+
+// Now vacation change
+vacationDays = 21;
